700ea7af66045c1b8e4d56edc9bacad777541602
layla-lili <laylaisa.211@gmail.com>
2021-02-02 00:25:13 +0300

    Task completed finally

--- src/App.js
+++ src/App.js
@@ -12,41 +12,43 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
-  const [textState, setTextState] = useState(stylings);
-  const [colorState, setColorState] = useState(colors);
-
-  const handleTextChange = (event) => {
-    setTextState({ value: event.target.value });
-    console.log(event.target.value);
-  };
-  const handleColorChange = (event) => {
-    setColorState({ value: event.target.value });
-  };
-
+  const [textState, setTextState] = useState({ textstyle: []});
+  const [colorState, setColorState] = useState({ textcolor: colors[0]});
+   
+  
+ 
+    
   const stylingBoxes = stylings.map((style) => (
     <button
-      onClick={() => handleTextChange}
+     
       className="btn btn-light"
       style={styles[style]}
       key={style}
+      onClick={() => setTextState(style)}
     >
       {style}
     </button>
   ));
 
   const colorBoxes = colors.map((color) => (
-    <button
-      onClick={() => handleColorChange}
+    <button 
+     
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
+        
+       onClick={() => setColorState(color)}
+    
     />
   ));
 
+
   return (
     <div className="App">
-      <div className="my-3">{stylingBoxes}</div>
+      <div className="my-3" >{stylingBoxes}</div>
+      
+      <textarea style={{ color: colorState, fontWeight: textState, fontStyle:textState, textDecorationLine:textState}} /> 
+    
 
-      <textarea />
       <div className="my-3">{colorBoxes}</div>
     </div>
   );
